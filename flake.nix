a4a8bc520dbfca25d50210f7e44e227fdeb315c0
Akshay <nerdy@peppe.rs>
2021-08-26 21:10:12 +0530

    attempt to add simple example

--- flake.nix
+++ flake.nix
@@ -27,25 +27,63 @@
 
       overlay = super: self: {
 
-        scion-path-discovery = super.buildGoModule rec {
-          pname = "scion-path-discovery";
+        simple-example = super.buildGoModule rec {
+          pname = "scion-simple-example";
           version = "0.1.0";
-          src = scion-src;
+          src = "${scion-src}";
+          subPackages = [
+            "examples/simple/simple.go"
+          ];
 
           vendorSha256 = "gWJWa6zNW5FnhqT4wTe0c28mmaHXM+dTUT4PLOMC1nA=";
+          # postConfigure = ''
+          #   [ -e "$TMPDIR" ] && ls -alh $TMPDIR
+          #   [ -e "$GOPATH" ] || export GOPATH="$TMPDIR/go"
+          #   [ -e "$GOBIN" ] || export GOBIN="$GOPATH/bin"
+          #   echo "$GOPATH" "$GOBIN"
+          #   mkdir -p "$GOBIN"
+          # '';
+          # installPhase = ''
+          #   runHook preInstall
+
+          #   mkdir -p $out
+          #   dir="$GOPATH/bin"
+          #   [ -e "$dir" ] && cp -r $dir $out
+
+          #   runHook postInstall
+          # '';
+          # postInstall = ''
+          #   ls -alh /build/go/bin
+          #   exit 1
+          # '';
+
           nativeBuildInputs = with super; [ ];
-          buildInputs = with super; [ go ];
         };
 
       };
 
       packages = forAllSystems (system: {
-        inherit (nixpkgsFor.${system}) scion-path-discovery;
+        inherit (nixpkgsFor.${system}) simple-example;
       });
+
+      apps = forAllSystems (system:
+        let
+          simple-example = (import nixpkgs {
+            inherit system;
+            overlays = [ self.overlay ];
+          }).simple-example;
+        in
+        {
+          simple-example = {
+            type = "app";
+            program = "${simple-example}/bin/simple";
+          };
+        }
+      );
 
 
       defaultPackage =
-        forAllSystems (system: self.packages.${system}.scion-path-discovery);
+        forAllSystems (system: self.packages.${system}.simple-example);
 
     };
 
