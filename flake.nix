62292e2f0faae99db99ae291131829e610498a2b
Martin Toman <toman.martin@live.com>
2022-08-31 15:27:40 -0700

    Cleanup

--- flake.nix
+++ flake.nix
@@ -9,53 +9,35 @@
     };
   };
 
+  outputs = { self, nixpkgs, scionSrc, ... }: let
+    supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+    forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
 
-  outputs = { self, nixpkgs, scionSrc, ... }:
-    let
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+    nixpkgsFor = forAllSystems (system: import nixpkgs {
+      inherit system;
+      overlays = [ self.overlays.default ];
+    });
 
-      nixpkgsFor = forAllSystems (system:
-        import nixpkgs {
-          inherit system;
-          overlays = [ self.overlays.default ];
-        }
-      );
-
-    in
-    {
-
-      overlays.default = final: prev: {
-
-        simple-example = with prev; callPackage ./buildExample.nix {
-          inherit buildGoModule scionSrc;
-          pname = "simple";
-          exampleSrc = "examples/simple/simple.go";
-          vendorSha256 = "gWJWa6zNW5FnhqT4wTe0c28mmaHXM+dTUT4PLOMC1nA=";
-        };
-
+  in {
+    overlays.default = final: prev: {
+      simple-example = with prev; callPackage ./buildExample.nix {
+        inherit buildGoModule scionSrc;
+        pname = "simple";
+        exampleSrc = "examples/simple/simple.go";
+        vendorSha256 = "gWJWa6zNW5FnhqT4wTe0c28mmaHXM+dTUT4PLOMC1nA=";
       };
-
-      packages = forAllSystems (system: rec {
-        inherit (nixpkgsFor.${system}) simple-example;
-        default = simple-example;
-      });
-
-      apps = forAllSystems (system:
-        let
-          simple-example = (import nixpkgs {
-            inherit system;
-            overlays = [ self.overlays.default ];
-          }).simple-example;
-        in
-        {
-          simple-example = {
-            type = "app";
-            program = "${simple-example}/bin/simple";
-          };
-        }
-      );
-
     };
 
+    packages = forAllSystems (system: rec {
+      inherit (nixpkgsFor.${system}) simple-example;
+      default = simple-example;
+    });
+
+    apps = forAllSystems (system: {
+      simple-example = {
+        type = "app";
+        program = "${self.packages.${system}.simple-example}/bin/simple";
+      };
+    });
+  };
 }
